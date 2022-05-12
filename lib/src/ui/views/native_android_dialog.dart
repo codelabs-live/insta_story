0d2d7b09df5038128b30f32a6b52b1039638b102
Kamlesh Kumar <dev.slinfy@gmail.com>
2022-05-12 17:33:52 +0530

    me

--- lib/src/ui/views/native_android_dialog.dart
+++ lib/src/ui/views/native_android_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class NativeAndroidDialog extends StatefulWidget {
   const NativeAndroidDialog({Key? key}) : super(key: key);
 
@@ -10,49 +11,32 @@ class _NativeAndroidDialogState extends State<NativeAndroidDialog> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(body: Center(
-    child: ElevatedButton(onPressed: (){
-showCostumeDialog();
-    },
-    child: Text("Click"),),
+    return Scaffold(
+        body: Center(
+      child: ElevatedButton(
+        onPressed: () {
+          showCostumeDialog(size);
+        },
+        child: Text("Click"),
+      ),
     ));
   }
 
-
-
-
-  showCostumeDialog(){
+  showCostumeDialog(Size size) {
     showDialog(
         context: context,
         builder: (BuildContext context) {
           return Dialog(
             shape: RoundedRectangleBorder(
-                borderRadius:
-                BorderRadius.circular(20.0)), //this right here
-            child: Container(
-              height: 200,
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
+                borderRadius: BorderRadius.circular(20.0)), //this right here
+            child: Card(
+              child: Container(
+                height: size.height*0.5,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'What do you want to remember?'),
-                    ),
-                    SizedBox(
-                      width: 320.0,
-                      child: RaisedButton(
-                        onPressed: () {},
-                        child: Text(
-                          "Save",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        color: const Color(0xFF1BC0C5),
-                      ),
-                    )
+
                   ],
                 ),
               ),
@@ -61,7 +45,3 @@ showCostumeDialog();
         });
   }
 }
-
-
-
-
