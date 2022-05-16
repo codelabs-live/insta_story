46d249b3f069e93abce418aaf7d797bd80ca8521
Kamlesh Kumar <dev.slinfy@gmail.com>
2022-05-16 12:35:14 +0530

    me

--- lib/src/ui/views/bottom_sheet.dart
+++ lib/src/ui/views/bottom_sheet.dart
@@ -8,26 +8,31 @@ class BottomSheetExample extends StatefulWidget {
 }
 
 class _BottomSheetExampleState extends State<BottomSheetExample> {
-
-
-
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery
-        .of(context)
-        .size;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Container(
-          width: size.width,
-          height: size.height,
-          child: Center(
-            child: ElevatedButton(onPressed: (){
-              showModalBottomSheet(context: context, builder: (context) =>
-                  Container(
-                    width:size.width,
-                  ));
-            }, child: Text("Click"),),
-          ),
-        ));
+        body: SizedBox(
+      width: size.width,
+      height: size.height,
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) => Container(
+                      width: size.width,
+                      child: ListView.builder(
+                        itemCount: 5,
+                        itemBuilder: (
+                                (context, index) => Padding(padding: EdgeInsets.all(10),
+                                child: Text("$index Item"))),
+                      ),
+                    ));
+          },
+          child: Text("Click"),
+        ),
+      ),
+    ));
   }
 }
